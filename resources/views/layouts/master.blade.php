3c35592c3b5c011ba926adf8ef965aa4d16ae160
adminlabo <adminlabo@192.168.226.143>
2017-03-16 16:29:15 +0100

    t00 SP00k f0R Y0u

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,7 +35,7 @@
                         <!--Navbar Brand-->
                         <a class="navbar-brand" href="{{ url('/') }}">GSB Commercial</a>
                         <!--Links-->
-                        @if (Session::get('id') == 0)
+                        @if (Session::get('id') < 0)
                         <ul class="nav navbar-nav">
                         </ul>
                         <ul class="nav navbar-nav" style="float: right;">
@@ -44,10 +44,14 @@
                             </li>
                         </ul>
                         @endif
-                        @if (Session::get('id') > 0)
+                        @if (Session::get('id') == 0)
                         <ul class="nav navbar-nav">
-                            <li><a href="{{ url('/liste') }}" data-toggle="collapse" data-target=".navbar-collapse.in">Lister</a></li>
-                            <li><a href="{{ url('/ajouter') }}" data-toggle="collapse" data-target=".navbar-collapse.in">Ajouter</a></li>  
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('lister') }}">Lister</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('ajouter') }}">Ajouter</a>
+                            </li> 
                         </ul>
                         <ul class="nav navbar-nav" style="float: right;">
                             <li class="nav-item">
--- resources/views/listeInvit.blade.php
+++ resources/views/listeInvit.blade.php
@@ -2,6 +2,28 @@
 @section('content')
 <div class="col-xs-12">
     <h4 class="gsb-titre">Liste</h4>
-    
+    <table class="table table-bordered table-striped table-responsive">
+        <thead>
+            <tr>
+                <th>Type Praticien</th> 
+                <th>Praticien</th> 
+                <th>Activité complémentaire</th>  
+                <th>Modifier</th>
+                <th>Supprimer</th> 
+            </tr>
+        </thead>
+        <!--@foreach($mesFrais as $unFrais)-->
+        <tr>   
+            <td> <!--{{ $unFrais->anneemois }}--> </td> 
+            <td style="text-align:center;"><a href="{{ url('/modifierFrais') }}/{{ $unFrais->id_frais }}">
+                    <span class="glyphicon glyphicon-pencil" data-toggle="tooltip" data-placement="top" title="Modifier"></span></a></td>
+            <td style="text-align:center;">
+                <a class="glyphicon glyphicon-remove" data-toggle="tooltip" data-placement="top" title="Supprimer" href="#"
+                   onclick="javascript:if (confirm('Suppression confirmée ?')){ window.location ='{{ url('/supprimerFrais')}}/'; }">
+                </a>
+            </td>                    
+        </tr>
+        <!--@endforeach-->
+    </table>
 </div>
 @stop
